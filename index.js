3b3efebf1b4ddf32822708ce6e9b2c5b67ed0d21
Christine Kim <christine@Christines-MacBook-Pro.local>
2017-10-21 02:48:40 -0500

    yay

--- index.js
+++ index.js
@@ -77,8 +77,20 @@ app.get('/webhook', (req, res) => {
     }
 });
 
-// Handles messages events
-function handleMessage(sender_psid, received_message) {
+    // Handles messages events
+    function handleMessage(sender_psid, received_message) {
+      let response;
+
+      // Check if the message contains text
+      if (received_message.text) {
+        // Create the payload for a basic text message
+        response = {
+          "test": 'You sent the message: "${received_message.text}". Now send me an image!'
+        }
+      }
+
+      // Sends the response message
+      callSendAPI(sender_psid, response);
 
     }
 
