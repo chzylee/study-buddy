35bd174d6900b51e4718e117ba3e59b34a623d72
David Lu <daiwei.lu@vanderbilt.edu>
2017-10-21 04:23:22 -0500

    updated comments

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.post('/webhook', (req, res) => {
         */
     } else {
         // Returns a '404 Not Found' if event is not from a page subscription
-        res.sendStatus(404);
+        res.sendStatus(303);
     }
 
 });
