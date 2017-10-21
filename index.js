f13344c6322bd63cdc3e5e3abbec9c23254b8c09
Christine Kim <christine@Christines-MacBook-Pro.local>
2017-10-21 04:32:04 -0500

    i

--- index.js
+++ index.js
@@ -27,28 +27,16 @@ app.post('/webhook', (req, res) => {
             let webhookEvent = entry.messaging[0];
             console.log(webhookEvent);
 
-            
-
             // Get the sender PSID
             let sender_psid = webhookEvent.sender.id;
             console.log('Sender PSID: ' + sender_psid);
-            /*
-            // Check if the event is a message or postback and
-            // pass the event to the appropriate handler function
-            if (webhookEvent.message) {
-              handleMessage(sender_psid, webhookEvent.message);
-            } else if (webhookEvent.postback) {
-              handlePostback(sender_psid, webhookEvent.postback);
-            } 
-            
         });
 
         // Returns a '200 OK' response to all requests
         res.status(200).send('EVENT_RECEIVED');
-        */
     } else {
         // Returns a '404 Not Found' if event is not from a page subscription
-        res.sendStatus(303);
+        res.sendStatus(404);
     }
 
 });
@@ -83,19 +71,7 @@ app.get('/webhook', (req, res) => {
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
-/*  let response;
 
-  // Check if the message contains text
-  if (received_message.text) {
-  // Create the payload for a basic text message
-    response = {
-      "text": 'You sent the message: "${received_message.text}". Now send me an image!'
-    }
-  }
-
-  // Sends the response message
-  callSendAPI(sender_psid, response);
-*/
 }
 
 // Handles messaging_postbacks events
@@ -105,26 +81,5 @@ function handlePostback(sender_psid, received_postback) {
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
- /* // Construct the message body
-  let request_body = {
-    "recipient": {
-      "id": sender_psid
-    },
-    "message": response
-  }
-
-  // Send the HTTP request to the Messenger Platform
-  request({
-    "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": PAGE_ACCESS_TOKEN },
-    "method": "POST",
-    "json": request_body
-  }, (err, res, body) => {
-    if (!err) {
-      console.log('message sent!')
-    } else {
-      console.error("Unable to send message:" + err);
-    }
-  });
-  */
+  
 }
