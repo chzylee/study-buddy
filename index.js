9092bafea1e86d65ee62f865349e38cb674927a8
Christine Kim <christine@Christines-MacBook-Pro.local>
2017-10-21 04:40:22 -0500

    b

--- index.js
+++ index.js
@@ -13,31 +13,31 @@ app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 // Creates the endpoint for our webhook
 app.post('/webhook', (req, res) => {
 
-     let body = req.body;
+  let body = req.body;
 
-    // Checks this is an event from a page subscription
-    if (body.object === 'page') {
-        // console.log(body); // for debugging
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+    // console.log(body); // for debugging
 
-        // Iterates over each entry - there may be multiple if batched
-        body.entry.forEach(function(entry) {
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
 
-            // Gets the message. entry.messaging is an array, but
-            // will only ever contain one message, so we get index 0
-            let webhookEvent = entry.messaging[0];
-            console.log(webhookEvent);
+      // Gets the message. entry.messaging is an array, but
+      // will only ever contain one message, so we get index 0
+      let webhookEvent = entry.messaging[0];
+      console.log(webhookEvent);
 
-            // Get the sender PSID
-            let sender_psid = webhookEvent.sender.id;
-            console.log('Sender PSID: ' + sender_psid);
-        });
+      // Get the sender PSID
+      let sender_psid = webhookEvent.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
+    });
 
-        // Returns a '200 OK' response to all requests
-        res.status(200).send('EVENT_RECEIVED');
-    } else {
-        // Returns a '404 Not Found' if event is not from a page subscription
-        res.sendStatus(404);
-    }
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+      // Returns a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
+  }
 
 });
 
@@ -45,7 +45,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
     // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "GZ99W5DZ8A"
+    let VERIFY_TOKEN = "GZ99W5DZ8A";
 
     // Parse the query params
     let mode = req.query['hub.mode'];
