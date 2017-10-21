1de13bbd72be0bd870c2112d44f362c13872edf1
Noah Lee <noahlee@Noahs-MacBook-Air.local>
2017-10-21 17:18:05 -0500

    prints out set

--- src/quizlet.js
+++ src/quizlet.js
@@ -1,47 +1,63 @@
-const request = require('request');
+const request = require('request-promise-native');
+
+async function searchSets(searchQuery) {
+    var options = {
+        method: 'GET',
+        uri: 'https://api.quizlet.com/2.0/search/sets',
+        headers: {
+            Authorization: 'Bearer ' + process.env.QUIZLET_ACCESS_TOKEN
+        },
+        qs: {
+            whitespace: true,
+            q: searchQuery,
+            per_page: 5
+        },
+        resolveWithFullResponse: true
+    };
+
+    const results = await request(options)
+        .then(function(response) {
+            // console.dir(JSON.parse(response.body));
+            var sets = JSON.parse(response.body).sets
+            return getSet(getSetId(sets));
+        });
+    return results;
+}
+
+function getSetId(sets) {
+    if(sets.length > 0) {
+        console.log(sets[0].id);
+        return sets[0].id;  
+    }
+    else {
+        console.log('Error: NO SETS FOUND');
+        return undefined;
+    }
+}
+
+async function getSet(id) {
+    if(id === undefined) {
+        return;
+    }
+    var options = {
+        method: 'GET',
+        uri: 'https://api.quizlet.com/2.0/sets/' + id,
+        headers: {
+            Authorization: 'Bearer ' + process.env.QUIZLET_ACCESS_TOKEN
+        }
+    };
+
+    const set = await request(options)
+    .then(function(response) {
+        // console.dir(JSON.parse(response));
+        var resp = JSON.parse(response);
+        console.log(resp.terms);
+    });
+    return set;
+}
 
 module.exports = {
-    searchSet(searchQuery) {
-        var options = {
-            method: 'GET',
-            uri: 'https://api.quizlet.com/2.0/search/sets',
-            headers: {
-                Authorization: 'Bearer GTph6BmbGUZ6PgcRx29STBMBhcJSbSp3pxeD47N4'
-            },
-            qs: {
-                whitespace: true,
-                q: searchQuery,
-                per_page: 5
-            }
-        };
-    
-        request(options, function(error, response, body) {
-            if(error){
-                console.log('error:');
-                console.log(error);
-            }
-            
-            console.log('response:');
-            // console.log(body);
-            var set = JSON.parse(body).sets[0];
-            console.log(set);
-        });
-    },
-    
-    getSet(id) {
-        var options = {
-            method: 'GET',
-            uri: 'https://api.quizlet.com/2.0/sets/' + id,
-            headers: {
-                Authorization: 'Bearer GTph6BmbGUZ6PgcRx29STBMBhcJSbSp3pxeD47N4'
-            }
-        };
-    
-        request(options, function(error, response, body) {
-            // console.log('error:');
-            // console.log(error);
-            console.log('set ' + id + ':');
-            console.log(JSON.parse(body).terms);
-        });
-    }
+    searchSets: searchSets,
+    getSetId: getSetId,
+    getSet: getSet
 }
