18b3ebff286967fb4016915614854630c941f919
Noah Lee <noahlee@Noahs-MacBook-Air.local>
2017-10-21 04:56:43 -0500

    add request module

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()); // creates express http server
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const request = require('request');
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
