abe0f5ba654a49a0b926091ccf2c2de5e170e401
Noah Lee <noahlee@Noahs-MacBook-Air.local>
2017-10-21 13:47:10 -0500

    reformatted heading

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 'use strict';
 
 // Imports dependencies and set up http server
-const
-    express = require('express'),
-    bodyParser = require('body-parser'),
-    app = express().use(bodyParser.json()); // creates express http server
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express().use(bodyParser.json()); // creates express http server
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const request = require('request');
 
