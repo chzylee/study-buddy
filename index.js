fe4abeaf64de1b8c1fcf9aa55b7cef7fa07b2f4b
Christine Kim <christine@Christines-MacBook-Pro.local>
2017-10-21 02:58:41 -0500

    update

--- index.js
+++ index.js
@@ -98,6 +98,20 @@ app.get('/webhook', (req, res) => {
           },
           "message": response
         }
+
+        // Send the HTTP request to the Messenger Platform
+        request({
+          "uri": "https://graph.facebook.com/v2.6/me/messages",
+          "qs": { "access_token": PAGE_ACCESS_TOKEN },
+          "method": "POST",
+          "json": request_body
+        }, (err, res, body) => {
+          if (!err) {
+            console.log('message sent!')
+          } else {
+            console.error("Unable to send message:" + err);
+          }
+        });
     }
 
     // Handles messaging_postbacks events
