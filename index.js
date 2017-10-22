400d494321b6624cab5603e6588f062c6b365afe
Noah Lee <noahlee@Noahs-MacBook-Air.local>
2017-10-21 19:37:27 -0500

    fixed spacing and brackets

--- index.js
+++ index.js
@@ -86,42 +86,40 @@ function handleMessage(sender_psid, received_message) {
 
     // Check if the message contains text
     if (received_message.text) {
-          // Create the payload for a basic text message
-          if (state === 'idle') {
-              // Check for greeting
-              const greetings = firstEntity(received_message.nlp, 'greetings');
-              if (greetings && greetings.confidence > 0.8) {
-                  console.log('detected greeting');
-                  response = {
-                      "text": 'Hey there! What would you like to study?'
-                  }
-              }
-          }   
-      } else if (received_message.attachments) {
-
-            // Gets the URL of the message attachment
-            let attachment_url = received_message.attachments[0].payload.url;
-            response = {
-                "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "template_type": "generic",
-                        "elements": [{
-                            "title": "Is this the right picture?",
-                            "subtitle": "Tap a button to answer.",
-                            "image_url": attachment_url,
-                            "buttons": [{
-                                "type": "postback",
-                                "title": "Yes!",
-                                "payload": "yes",
-                            },
-                            {
-                                "type": "postback",
-                                "title": "No!",
-                                "payload": "no",
-                            }],
-                        }]
-                    }
+        // Create the payload for a basic text message
+        if (state === 'idle') {
+            // Check for greeting
+            const greetings = firstEntity(received_message.nlp, 'greetings');
+            if (greetings && greetings.confidence > 0.8) {
+                console.log('detected greeting');
+                response = {
+                    "text": 'Hey there! What would you like to study?'
+                }
+            }
+        }   
+    } else if (received_message.attachments) {
+        // Gets the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Is this the right picture?",
+                        "subtitle": "Tap a button to answer.",
+                        "image_url": attachment_url,
+                        "buttons": [{
+                            "type": "postback",
+                            "title": "Yes!",
+                            "payload": "yes",
+                        },
+                        {
+                            "type": "postback",
+                            "title": "No!",
+                            "payload": "no",
+                        }],
+                    }]
                 }
             }
         }
