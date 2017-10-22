c5fa3294fb62a3b61893af75d25767c326f9df7b
Noah Lee <noahlee@Noahs-MacBook-Air.local>
2017-10-22 01:03:58 -0500

    flashcard funcs

--- src/flashcards.js
+++ src/flashcards.js
@@ -1,14 +1,92 @@
 const quizlet = require('./quizlet');
-const store = require('./store');
+// const store = require('./store');
 
 module.exports = class Flashcards {
     constructor() {
-        this.dataStore = new store();
+       this.question = '';
     }
 
     // gets set of flashcards to use for user with given search query
-    getFlashcards(searchQuery) {
-        // find sets
-        quizlet.searchSets(searchQuery, this.dataStore);
+    async searchFlashcards(searchQuery) {
+        try {
+            var setID = await quizlet.searchSets(searchQuery);
+            return this.getFlashcards(setID);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async getFlashcards(id) {
+        try {
+            var set = await quizlet.getSet(id);
+            return this.parseFlashcards(set);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    // parse deck of cards to thin data
+    parseFlashcards(set) {
+        var parsedSet = [];
+        for (var i = 0; i < set.length; i++) {
+            parsedSet.push({
+                term: set[i].term,
+                definition: set[i].definition,
+                image: set[i].image.url
+            });
+        }
+        // console.log('PARSED SET:');
+        // console.log(parsedSet);
+        // console.log(set.length);
+        return parsedSet;
+    }
+
+    getTermQuestion(set) {
+        // var set = getCurrentSet();
+        if (set.length === 0) {
+            return 'You have used all the cards in this set.';
+        }
+        this.question = { // populate answer data with info from set
+            q: 'What is the definition of this term: ' + set[0].term + '?',
+            a: set[0].definition
+        }
+        set = set.shift(); // pop card off set
+        return this.question.q;
+    }
+
+    getDefinitionQuestion(set) {
+        // var set = getCurrentSet();
+        if (set.length === 0) {
+            return 'You have used all the cards in this set.';
+        }
+        this.question = {
+            q: 'What is the term corresponding to this definition: ' + set[0].definition + '?',
+            a: set[0].term
+        }
+        set = set.shift(); // pop card off set
+        return this.question.q;
+    }
+
+    getImageQuestion(set) {
+        // var set = getCurrentSet();
+        if (set.length === 0) {
+            return 'You have used all the cards in this set.';
+        }
+        this.question = {
+            q: 'What is the term corresponding to this image: ' + set[0].image + '?',
+            a: set[0].term
+        }
+        set = set.shift(); // pop card off set
+        return this.question.q;
+    }
+    
+    getAnswer(guess) {
+        // var question = this.dataStore.getData('answer');
+        if (guess === this.question.a) {
+            return 'Correct! :)';
+        }
+        else {
+            return 'Incorrect. :(';
+        }
     }
 }
